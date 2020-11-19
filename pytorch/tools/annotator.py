1f535bc1fa02ef9ebddf8cdc020efa15557efcda
mousyball <mouse801117@gmail.com>
2020-11-19 13:56:54 +0800

    [refactor] Make annotator workable
    * Remove unused parameters
    * Remove meaningless comments

--- pytorch/tools/annotator.py
+++ pytorch/tools/annotator.py
@@ -10,7 +10,7 @@ import numpy as np
 import torch
 from PIL import Image
 
-from fbrs.isegm.inference import utils
+from core.isegm.inference import utils
 from core.model_controller import InteractiveController
 from glob import glob
 
@@ -39,7 +39,6 @@ class Annotator(object):
         self.merge =  self.__gene_merge(self.pred, self.img, self.clicks)
         self.predictor_params = predictor_params
 
-        # TODO:
         self.controller = controller
         self.controller.set_image(self.img)
         self.img_list = [img_path] if img_list is None else img_list
@@ -66,13 +65,12 @@ class Annotator(object):
         self.clicks =  np.empty([0,3],dtype=np.int64)
         self.pred = np.zeros(self.img.shape[:2],dtype=np.uint8)
         self.merge =  self.__gene_merge(self.pred, self.img, self.clicks)
-        # TODO:
+
         self.controller.set_image(self.img)
         self.controller.reset_last_object(self.predictor_params)
         self.__update()
 
     def __predict(self):
-        # TODO:
         self.pred = (self.controller.current_object_prob > 0.5).astype(np.uint8) * 255
         self.merge =  self.__gene_merge(self.pred, self.img, self.clicks)
         self.__update()
@@ -99,13 +97,13 @@ class Annotator(object):
 
         if event.key=='ctrl+z':
             self.clicks=self.clicks[:-1,:]
-            self.controller.undo_click() # TODO:
+            self.controller.undo_click()
             if len(self.clicks)!=0:
                 self.__predict()
             else:
                 self.__reset()
         elif event.key=='ctrl+r':
-            self.controller.reset_last_object(self.predictor_params) # TODO:
+            self.controller.reset_last_object(self.predictor_params)
             self.__reset()
         elif event.key=='escape':
             plt.close()
@@ -121,7 +119,7 @@ class Annotator(object):
         if event.button==1 or  event.button==3:
             x,y= int(event.xdata+0.5), int(event.ydata+0.5)
             self.clicks=np.append(self.clicks,np.array([[x,y,(3-event.button)/2]],dtype=np.int64),axis=0)
-            # TODO:
+
             self.controller.add_click(
                 x=self.clicks[-1][0],
                 y=self.clicks[-1][1],
@@ -143,7 +141,7 @@ class Annotator(object):
 def parse_args():
     parser = argparse.ArgumentParser(description="Annotator")
     parser.add_argument('--checkpoint', type=str,
-                        default='./fbrs/weights/hrnet32_ocr128_lvis.pth',
+                        default='./weights/hrnet32_ocr128_lvis.pth',
                         help='The path to the checkpoint. '
                              'This can be a relative path (relative to cfg.INTERACTIVE_MODELS_PATH) '
                              'or an absolute path. The file extension can be omitted.')
@@ -165,11 +163,8 @@ def parse_args():
 
 
 def init(args):
-    # Possible choices: 'NoBRS', 'f-BRS-A', 'f-BRS-B', 'f-BRS-C', 'RGB-BRS', 'DistMap-BRS'
-    USE_ZOOM_IN = False
     MAX_CLICKS = 20
     MODEL_THRESH = 0.5
-    MODE = args.mode
 
     device = torch.device("cuda:"+str(args.gpu_id)
                           if torch.cuda.is_available()
@@ -184,24 +179,13 @@ def init(args):
 
     # NOTE: Parameters
     params = {
-        'zoom_in_params': {
-            'use_zoom_in': USE_ZOOM_IN,
-            'skip_clicks': 1,
-            'target_size': 600,
-            'expansion_ratio': 1.4
-        },
         'predictor_params': {
             'net_clicks_limit': MAX_CLICKS
-        },
-        'brs_mode': MODE,
-        'prob_thresh': MODEL_THRESH,
-        #'lbfgs_max_iters': 20
+        }
     }
 
     # NOTE: image_list to be verified
-    img_list = ['images/17790319373_bd19b24cfc_k.jpg', 'images/sheep.jpg', 'images/bear.jpg']
-    img_list = glob(r'./images/50/*')
-    # img_list = glob(r'/home/cilin/pytorch_repo/pycococreator/projects/human17/images/official_125/125_100_00-01_00/*') # blur, dark
+    img_list = glob(r'./images/*')
 
     ret_dict = dict(
         net=model,
@@ -217,7 +201,7 @@ if __name__ == "__main__":
     args = parse_args()
     cfg = init(args)
     controller = InteractiveController(**cfg)
-    img_path = './images/frame0025.jpg'
+    img_path = './images/bear.jpg'
 
     anno = Annotator(
         img_path=img_path,
